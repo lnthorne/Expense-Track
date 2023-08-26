c7a071a70069bd620f1d219ffeeb118ad6bdafed
Liam Thorne <lnthorne@shaw.ca>
2023-08-26 13:53:07 -0700

    Create server and connect DB

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,16 @@
-const greeting = (name: string): string => {
-	return `Hello, ${name}!`;
-};
+import express from "express";
+import connectDB from "./database";
+import "dotenv/config";
 
-console.log(greeting("Node with TypeScript"));
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+connectDB();
+
+app.get("/", (req, res) => {
+	res.send("Hello World!");
+});
+
+app.listen(PORT, () => {
+	console.log(`Server listening on port ${PORT}`);
+});
